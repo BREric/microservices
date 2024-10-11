3bd089c424334b99be240ffb0e4cd0a115d7a068
BREric <eric.bedoyar@uqvirtual.edu.co>
2024-10-10 23:11:49 -0500

    acceso a mongodb

--- logs_service/app/database.py
+++ logs_service/app/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 def init_db():
-    client = MongoClient("mongodb://mongodb:27017") 
+    client = MongoClient("mongodb://mongodb_container:27017") 
     db = client['logs_service']
     return db
 
